c0cf39357f8e817acc451dda06779a116a610dc8
zhcode <zhlearn@sina.com>
2020-06-23 10:47:17 +0800

    merge

---
+++ src/pages/detail/index.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="detail">
+    detail
+     <!-- <Calendar
+      
+      :value="value"
+      @next="next"
+      @prev="prev"
+      @select="select"
+      ref="calendar"
+      @selectMonth="selectMonth"
+      @selectYear="selectYear"
+    /> -->
+  <div class="" @click="goBack">返回</div>
+  </div>
+</template>
+<script>
+import Calendar from 'mpvue-calendar'
+import 'mpvue-calendar/src/style.css'
+import DateCalendar from 'vuejs-date-calendar'
+// import Calendar from 'mpvue-calendar' //
+// import 'mpvue-calendar/src/style.css' //引入样式文件(mpvue 小程序端)
+export default {
+  data() {
+    return {
+      show:true,
+    }
+  },
+   components: {
+    Calendar,
+    vDateCalendar:DateCalendar
+  },
+  methods: {
+    goBack(){
+      let pages =  getCurrentPages();
+      let curPage = pages[pages.length - 1]
+      let prevPage = pages[pages.length - 2]
+      console.log(prevPage);
+      prevPage.data.$root[0].setData('xiaoqu','ddd')
+      // prevPage.setData({
+      //   '$root[0].nih':'ddd'
+      // })
+      wx.navigateBack({
+        delta: 1
+      });
+    },
+    prev(year, month, weekIndex) {
+      console.log(year, month, weekIndex)
+    },
+    next(year, month, weekIndex) {
+      console.log(year, month, weekIndex)
+    },
+    selectYear(year) {
+      console.log(year)
+    },
+    selectMonth(month, year) {
+      console.log(year, month)
+    },
+    setToday() {
+      this.$refs.calendar.setToday()
+    },
+    dateInfo() {
+      const info = this.$refs.calendar.dateInfo(2018, 8, 23)
+      console.log(info)
+    },
+    renderer() {
+      this.$refs.calendar.renderer(2018, 8); //渲染2018年8月份
+    },
+    select(val, val2) {
+      console.log(val)
+      console.log(val2)
+    }
+  },
+  mounted () {
+    // let app = getApp()
+    mpvue.setNavigationBarTitle({
+      title: '当前页面'
+    })
+  }
+}
+</script>
+<style>
+.detail {
+}
+</style>
---
+++ src/pages/invatation/index.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="invatation">
+    <div class="form">
+      <div class="item" @click="goNav('../detail/main')">
+        <label class="label">小区名称</label>
+        <p class="inpt">{{xiaoqu}}</p>
+        
+      </div>
+      <div class="item">
+        <label class="label">钥匙名称</label>
+        <p class="inpt">{{xiaoqu}}</p>
+        <span></span>
+      </div>
+      <div class="item">
+        <label class="label">来访时间</label>
+        <p class="inpt">{{other}}</p>
+      </div>
+      <div class="item">
+        <label class="label"></label>
+        <p class="inpt">结束时间</p>
+      </div>
+      <div class="item">
+        <label class="label">访客手机</label>
+        <input class="inpt" type="text" placeholder="请输入手机号码" placeholder-style="color: #9EA6B7;">
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+let that = null;
+export default {
+  data() {
+    return {
+      xiaoqu: "请选择小区",
+      other: 'kkk',
+      setData: function (key,value) {
+        that[key] = value
+      }
+    }
+  },
+  mounted () {
+   that = this;
+},
+  methods:{
+    goNav(url){
+      this.other = " nihao"
+      mpvue.navigateTo({ url })
+    }
+  }
+}
+</script>
+<style>
+.invatation {
+}
+.form .item{
+  display: flex;
+  padding: 15px 20px;
+}
+.form .item:not(:first-child){
+  border-top: 1px solid #F4F6F8;
+}
+.form .label{
+  width: 85px;
+  font-size: 15px;
+  line-height: 21px;
+}
+.form .icon{
+  flex: 1;
+  text-align: right;
+}
+.inpt {
+  font-size: 14px;
+  line-height: 20px;
+  color: #9EA6B7;
+}
+</style>
